6d5d00dd40856bd067cdc335125f239da3e4cd6a
Eric Denovellis <edeno@bu.edu>
2017-10-24 17:33:35 -0400

    Add animals

--- src/parameters.py
+++ src/parameters.py
@@ -10,6 +10,16 @@ Animal = namedtuple('Animal', {'directory', 'short_name'})
 ANIMALS = {
     'HPa': Animal(directory=join(RAW_DATA_DIR, 'HPa_direct'),
                   short_name='HPa'),
+    'HPb': Animal(directory=join(RAW_DATA_DIR, 'HPb_direct'),
+                  short_name='HPb'),
+    'HPc': Animal(directory=join(RAW_DATA_DIR, 'HPc_direct'),
+                  short_name='HPc'),
+    'bon': Animal(directory=join(RAW_DATA_DIR, 'Bond'),
+                  short_name='bon'),
+    'Fra': Animal(directory=join(RAW_DATA_DIR, 'frank'),
+                  short_name='fra'),
+    'Cor': Animal(directory=join(RAW_DATA_DIR, 'CorrianderData'),
+                  short_name='Cor'),
 }
 
 REPLAY_COVARIATES = ['session_time', 'replay_task',
