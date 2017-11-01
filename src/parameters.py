3a64b8365aa7a7063e0665a22e09e3954b43b3bd
Eric Denovellis <edeno@bu.edu>
2017-11-01 10:34:32 -0400

    Add some comments

--- src/parameters.py
+++ src/parameters.py
@@ -1,7 +1,12 @@
 from collections import namedtuple
 from os.path import join, abspath, dirname, pardir
 
+import seaborn as sns
+
+# LFP sampling frequency
 SAMPLING_FREQUENCY = 1500
+
+# Data directories and definitions
 ROOT_DIR = join(abspath(dirname(__file__)), pardir)
 RAW_DATA_DIR = join(ROOT_DIR, 'Raw-Data')
 PROCESSED_DATA_DIR = join(ROOT_DIR, 'Processed-Data')
